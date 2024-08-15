a8f1ec27bcd6ceb6259185477833e68f31281ce7
MD Shourav Hossain <89313694+rhshourav@users.noreply.github.com>
2024-08-15 12:22:35 +0600

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -8,10 +8,7 @@ def generate_readme():
     # Get the current time in Bangladesh time
     bd_time = datetime.datetime.now(bd_timezone)
     
-    content = f"![](https://komarev.com/ghpvc/?username=rhshourav&color=03fca9)"
-    content += f""" \n\n<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rhshourav&size_weight=0.0010&count_weight=0.6&theme=dracula&border_color=03fca9&langs_count=10&card_width=320&layout=pie">"""
-    content +=  f"\n\n[![GitHub Streak](https://streak-stats.demolab.com?user=rhshourav&theme=nordfox&hide_border=true&border_radius=4.6&card_width=320)](https://git.io/streak-stats)"
-    content += f"""
+    content = f"""
 <!-- TryHackMe Badge -->
 <div id="thm_badge" style="display: inline-table; width: 220px; margin: 5px; cursor: pointer;" onclick="location.href='https://tryhackme.com/p/deshoha'">
   <div style="display: flex; align-items: center;">
@@ -32,8 +29,11 @@ def generate_readme():
   </div>
 </div>
 """
+    content += f"![](https://komarev.com/ghpvc/?username=rhshourav&color=03fca9)"
     content += f"\n\nLast updated: {bd_time.strftime('%hh:%hh:%ss %I:%M:%S %p ')} \n"
-
+    content += f""" \n\n<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rhshourav&size_weight=0.0010&count_weight=0.6&theme=dracula&border_color=03fca9&langs_count=10&card_width=320&layout=pie">"""
+    content +=  f"\n\n[![GitHub Streak](https://streak-stats.demolab.com?user=rhshourav&theme=nordfox&hide_border=true&border_radius=4.6&card_width=320)](https://git.io/streak-stats)"
+    
     with open("README.md", "w") as readme_file:
         readme_file.write(content)
 
