5366b1a5cc13a811131879d5f5a054735fc55d7a
MD Shourav Hossain <89313694+rhshourav@users.noreply.github.com>
2024-08-15 12:41:28 +0600

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -29,7 +29,7 @@ def generate_readme():
   </div>
 </div>
 """
-    content += f"![](https://komarev.com/ghpvc/?username=rhshourav&color=03fca9)"
+    content += f"\n\n![](https://komarev.com/ghpvc/?username=rhshourav&color=03fca9)"
     content += f"\n\nLast updated: {bd_time.strftime('%hh:%hh:%ss %I:%M:%S %p ')} \n"
     content += f""" \n\n<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rhshourav&size_weight=0.0010&count_weight=0.6&theme=dracula&border_color=03fca9&langs_count=10&card_width=320&layout=pie">"""
     content +=  f"\n\n[![GitHub Streak](https://streak-stats.demolab.com?user=rhshourav&theme=nordfox&hide_border=true&border_radius=4.6&card_width=320)](https://git.io/streak-stats)"
