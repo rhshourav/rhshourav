8f83f8d25e2387777e3f5926bb13fed57959faf7
MD Shourav Hossain <89313694+rhshourav@users.noreply.github.com>
2024-08-15 12:15:45 +0600

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -11,7 +11,6 @@ def generate_readme():
     content = f"![](https://komarev.com/ghpvc/?username=rhshourav&color=03fca9)"
     content += f""" \n\n<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rhshourav&size_weight=0.0010&count_weight=0.6&theme=dracula&border_color=03fca9&langs_count=10&card_width=320&layout=pie">"""
     content +=  f"\n\n[![GitHub Streak](https://streak-stats.demolab.com?user=rhshourav&theme=nordfox&hide_border=true&border_radius=4.6&card_width=320)](https://git.io/streak-stats)"
-    content += f"\n \n  ![TryHackMe](https://tryhackme.com/badge/533634)"
     content += f"""
 <!-- TryHackMe Badge -->
 <div id="thm_badge" style="display: inline-table; width: 220px; margin: 5px; cursor: pointer;" onclick="location.href='https://tryhackme.com/p/deshoha'">
@@ -28,7 +27,7 @@ def generate_readme():
         <img src="https://assets.tryhackme.com/img/badges/target.png" alt="target" style="height: 16px; opacity: 0.85; margin-left: 5px;" />
         <span style="color: #ffffff; font-size: 14px; opacity: 0.5;">16</span>
       </div>
-      <a href="https://www.tryhackme.com" style="color: #fff; font-size: 10px; text-decoration: none; position: absolute; bottom: 5px; right: 5px;">tryhackme.com</a>
+      <a href="https://www.tryhackme.com/deshoha" style="color: #fff; font-size: 10px; text-decoration: none; position: absolute; bottom: 5px; right: 5px;">tryhackme.com</a>
     </div>
   </div>
 </div>
